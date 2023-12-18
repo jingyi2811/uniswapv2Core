c4cb14b0e74c5ac9ad35659731bc562369e6ab57
jingyi2811 <jingyi2811@gmail.com>
2023-12-18 13:23:54 +0800

    Update

--- test/stablePool.t.sol
+++ test/stablePool.t.sol
@@ -8,19 +8,21 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
 contract StablePoolTest is Test {
-    address dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-    address factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    address daiAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address usdtAddress = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address factoryAddress = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
 
     IUniswapV2Factory factory;
     IUniswapV2Pair pair;
 
     function setUp() public {
-        factory = IUniswapV2Factory(factory);
-        pair = IUniswapV2Pair(factory.getPair(dai, usdt));
+        factory = IUniswapV2Factory(factoryAddress);
+        pair = IUniswapV2Pair(factory.getPair(daiAddress, usdtAddress));
     }
 
     function testStablePool() public {
-
+        (uint112 x, uint112 y, )= pair.getReserves();
+        console.log(x);
+        console.log(y);
     }
 }
